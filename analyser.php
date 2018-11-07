1a3b41acf33a01c8c2f9a6fc60b652684b8e3442
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-11-07 09:27:21 +0100

    get data on file 1

--- analyser.php
+++ analyser.php
@@ -1,7 +1,13 @@
 <?php
 
+$full_data=[];
+
 $file = fopen('data/task1-train.csv', 'r');
-while (($line = fgetcsv($file)) !== FALSE) {
-   print_r($line);
-}
+while (($line = fgetcsv($file,0,"\t")) !== FALSE) {
+	if (count($line)==3) {
+		array_push($full_data, $line);
+	}
+  }
 fclose($file);
+
+var_dump($full_data); die;
