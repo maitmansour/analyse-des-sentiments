75678aa5f869f17e193e20a429fa0f870da1869b
Mohamed AIT MANSOUR - UBUNTU <mohamedaitmansour95@gmail.com>
2018-11-07 10:04:53 +0100

    reformat data to match <label> <index>:<rep>

--- analyser.php
+++ analyser.php
@@ -2,6 +2,8 @@
 
 $full_data=[];
 $words=[];
+$index=1;
+
 $file = fopen('data/task1-train.csv', 'r');
 while (($line = fgetcsv($file,0,"\t")) !== FALSE) {
 	if (count($line)==3) {
@@ -20,8 +22,54 @@ fclose($file);
 
 foreach ($full_data as $key => $value) {
 	$tmp_words=explode(" ", $value[1]);
-	$words=array_merge($words,$tmp_words);
-}
-$unique_values = array_count_values($words);
 
-var_dump($unique_values); die;
+	switch ($value[2]) {
+		case 'objective':
+			$label="0";
+			break;
+		case 'positif':
+			$label="1";
+			break;
+		case 'negative':
+			$label="2";
+			break;
+		case 'mixte':
+			$label="3";
+			break;
+		default:
+			continue;
+	}
+
+	$tmp_pair="";
+	foreach ($tmp_words as $key => $value) {
+		if (isset($words[$value])) {
+			$index_word=$words[$value];
+		}else{
+			$words[$value]=$index;
+			$index_word=$index;
+			$index++;
+		}
+
+		$tmp_repetition=0;
+		foreach ($tmp_words as $keyTmp => $valueTmp) {
+			if ($value==$valueTmp) {
+				$tmp_repetition++;
+			}
+		}
+
+		$tmp_pair.=$index_word.":".$tmp_repetition." ";
+		/*echo $tmp_pair;
+			if ($index==40) {
+			die;
+		}*/
+
+	}
+
+	//$words=array_merge($words,$tmp_words);
+
+	echo $label." ".$tmp_pair."<br>";
+			if ($index==2) {
+			die;
+		}
+
+}
